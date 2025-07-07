53b6e80727e589bb0a8a24a3fb15dc0812225d76
Gaurav Mishra <gauravmishra@Gauravs-MacBook-Air.local>
2025-07-07 19:24:05 +0530

    ADDING spark RDD code

--- SparkBasicRDD3.py
+++ SparkBasicRDD3.py
@@ -32,8 +32,14 @@ if __name__ == '__main__':
    rdd3 = rdd2.filter(lambda x: x[3] == "CLOSED")
    #rdd3.takeOrdered(2)): [(10, '2013-07-25 00:00:00.0', '11599', 'CLOSED'), (40, '2013-07-25 00:00:00.0', '8827', 'CLOSED')]
   
+   # countByValue() returns a dictionary with the count of each unique value in the RDD
+   print(rdd3.map(lambda x: x[3]).countByValue())
+
    print("rdd2", rdd2.count(), "rdd3", rdd3.count())
    rdd4 = rdd3.map(lambda x: (x[2], 1))
    rdd5 = rdd4.reduceByKey(lambda x, y: x + y)
+
+   rdd6 = spark.sparkContext.parallelize((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+   print("rdd6", rdd6.reduce(lambda x, y: x + y))  # Output: 55
 
    rdd5.saveAsTextFile("/Users/gauravmishra/Desktop/Adding/SparkRDD/Output/orders_closed.txt")
